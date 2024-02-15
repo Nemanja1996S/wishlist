439c425bb2aeae7970b81e857e2089c9e3b915af
Nemanja1996S <savic1996.neca@gmail.com>
2024-02-15 18:31:59 +0100

    Decisions & prop bind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { wishItem } from '../shared/models/wishItem';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor],
+  imports: [RouterOutlet, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'My wishlist';
-  items = [
+  items : wishItem[] = [
     new wishItem('Learn Angular'),
     new wishItem('Get a student intership'),
+    new wishItem('Come up with topic for final thesis', true),
     new wishItem('Get a developer job')
   ]
 }
