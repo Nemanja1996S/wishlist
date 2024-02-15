058cd134814917245a3bbbd66c0e7e969fdb48ca
Nemanja1996S <savic1996.neca@gmail.com>
2024-02-15 15:09:22 +0100

    Part 2,3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { wishItem } from '../shared/models/wishItem';
+import { NgFor } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'wishlist';
+  title = 'My wishlist';
+  items = [
+    new wishItem('Learn Angular'),
+    new wishItem('Get a student intership'),
+    new wishItem('Get a developer job')
+  ]
 }
