43fe96cdccf27053fbdbbc6bffeacb349da87b31
Nemanja1996S <savic1996.neca@gmail.com>
2024-02-20 17:51:30 +0100

    Creating routes & handling bad routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { WishComponent } from './wish/wish.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+export const routes: Routes = [
+    { path : '', component: WishComponent },
+    { path : 'contact', component: ContactComponent },
+    { path : '**', component: NotFoundComponent}
+];
